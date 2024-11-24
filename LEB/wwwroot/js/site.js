60b55ecd66962bfb1ea00c937a753fb04d66dc00
Léo Trinquet <leo.trinquet@universite-paris-saclay.fr>
2024-11-24 18:01:49 +0100

    Added deleting notes feature

---
+++ LEB/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿function closeModal() {
+    const modalElement = document.getElementById('myModal');
+    const modalInstance = bootstrap.Modal.getInstance(modalElement);
+    if (modalInstance) {
+        modalInstance.hide();
+    }
+}
