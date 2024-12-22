7778840b10904b1b4cccb0dda5e6521103cdab2d
Ali <alilena999@gmail.com>
2024-12-22 19:22:40 +0100

    LLM Query integration and displaying

---
+++ LEB/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿function closeModal() {
+    const modalElement = document.getElementById('myModal');
+    const modalInstance = bootstrap.Modal.getInstance(modalElement);
+    if (modalInstance) {
+        modalInstance.hide();
+    }
+}
+
+window.OpenModalWithAnimation = (modalId) => {
+    var modalElement = document.querySelector(modalId);
+
+    if (!modalElement) return; 
+
+    var modal = new bootstrap.Modal(modalElement, {
+        backdrop: true,
+        keyboard: true
+    });
+
+    $(modalElement).removeClass('fade').addClass('fade');
+    modal.show();
+};
+
+window.CloseModal = (modalId) => {
+    var modalElement = document.querySelector(modalId);
+    if (modalElement) {
+        var modal = bootstrap.Modal.getInstance(modalElement);
+        if (modal) {
+            modal.hide();
+        }
+    }
+}
+
+
